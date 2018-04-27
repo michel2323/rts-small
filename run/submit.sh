6f131d2ecc81be5556ad6ef661995403d1bd1d57
Michel Schanen <mschanen@anl.gov>
2018-04-27 14:21:45 +0000

    batch submission scripts

--- run/submit.sh
+++ run/submit.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+ltime=120
+scenarios=$1 
+ppn=32
 
-nodes=(10 20) 
+cd ..
+path=$PWD
+cd run
 
-for i in "${nodes[@]}"
-do
-  sed "s/NODES/$i/g" job_template.sh > job.sh
-  chmod 755 job.sh
-  qsub job.sh
-done
+# ceil. We use 2 scenarios per core (*2).
+nodes=$((($scenarios + $ppn*2 - 1)/($ppn*2)))
+sed "s,PWDPATH,$path,g" job_template.sh | sed "s/NODES/$nodes/g" | sed "s/TIME/$ltime/g" | sed "s/SCENARIOS/$scenarios/g" > job.sh
+echo $nodes
+chmod 755 job.sh
+qsub job.sh
